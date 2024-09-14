3aefda85524be7ab3185c2156ea7e8bcd3e6c500
Ignacio Silva <ignacio.silva.e@gmail.com>
2024-09-14 17:23:17 -0300

    Add constants path and move front/back stack to constants folder

---
+++ src/constants/backendStack.ts
@@ -0,0 +1,58 @@
+//Backend stack
+import NodeJs from "@icons/stack/backend/Nodejs.svg?raw";
+import ExpressJs from "@icons/stack/backend/Expressjs.svg?raw";
+import Mongodb from "@icons/stack/backend/Mongodb.svg?raw";
+import PostgreSQL from "@icons/stack/backend/PostgreSQL.svg?raw";
+import Docker from "@icons/stack/backend/Docker.svg?raw";
+import Golang from "@images/icons/stack/backend/Golang.svg?raw";
+
+export const GOLANG = {
+  Title: "Golang",
+  website: "https://go.dev/",
+  icon: Golang,
+  className: "",
+};
+
+export const NODEJS = {
+  Title: "NodeJs",
+  website: "https://nodejs.org/",
+  icon: NodeJs,
+  className: "",
+};
+
+export const EXPRESSJS = {
+  Title: "ExpressJs",
+  website: "https://expressjs.com/",
+  icon: ExpressJs,
+  className: "",
+};
+
+export const MONGODB = {
+  Title: "MongoDB",
+  website: "https://www.mongodb.com/",
+  icon: Mongodb,
+  className: "",
+};
+
+export const POSTGRESQL = {
+  Title: "PostgreSQL",
+  website: "https://www.postgresql.org/",
+  icon: PostgreSQL,
+  className: "",
+};
+
+export const DOCKER = {
+  Title: "Docker",
+  website: "https://www.docker.com/",
+  icon: Docker,
+  className: "",
+};
+
+export const BACKEND_STACK = [
+  GOLANG,
+  NODEJS,
+  EXPRESSJS,
+  MONGODB,
+  POSTGRESQL,
+  DOCKER,
+];
---
+++ src/constants/frontendStack.ts
@@ -0,0 +1,76 @@
+//Frontend stack
+import NextJs from "@icons/stack/frontend/Nextjs.svg?raw";
+import AntDesign from "@icons/stack/frontend/AntDesign.svg?raw";
+import Redux from "@icons/stack/frontend/Redux.svg?raw";
+import ReduxSaga from "@icons/stack/frontend/ReduxSaga.svg?raw";
+import Tailwind from "@icons/stack/frontend/Tailwind.svg?raw";
+import Axios from "@icons/stack/frontend/Axios.svg?raw";
+import GraphQL from "@icons/stack/frontend/GraphQL.svg?raw";
+import ApolloClient from "@icons/stack/frontend/ApolloClient.svg?raw";
+
+export const NEXTJS = {
+  Title: "Next.js",
+  website: "https://nextjs.org/",
+  icon: NextJs,
+  className: "dark:fill-white",
+};
+
+export const ANT_DESIGN = {
+  Title: "Ant Design",
+  website: "https://ant.design/",
+  icon: AntDesign,
+  className: "",
+};
+
+export const REDUX = {
+  Title: "Redux",
+  website: "https://redux.js.org/",
+  icon: Redux,
+  className: "",
+};
+
+export const REDUX_SAGA = {
+  Title: "Redux Saga",
+  website: "https://redux-saga.js.org/",
+  icon: ReduxSaga,
+  className: "",
+};
+
+export const TAILWIND = {
+  Title: "Tailwind CSS",
+  website: "https://tailwindcss.com/",
+  icon: Tailwind,
+  className: "",
+};
+
+export const AXIOS = {
+  Title: "Axios",
+  website: "https://axios-http.com/",
+  icon: Axios,
+  className: "",
+};
+
+export const GRAPHQL = {
+  Title: "GraphQL",
+  website: "https://graphql.org/",
+  icon: GraphQL,
+  className: "",
+};
+
+export const APOLLO_CLIENT = {
+  Title: "Apollo Client",
+  website: "https://www.apollographql.com/docs/react/",
+  icon: ApolloClient,
+  className: "",
+};
+
+export const FRONTEND_STACK = [
+  NEXTJS,
+  ANT_DESIGN,
+  REDUX,
+  REDUX_SAGA,
+  TAILWIND,
+  AXIOS,
+  GRAPHQL,
+  APOLLO_CLIENT,
+];
