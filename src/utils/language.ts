e02c5ca8dd05711a343237b5c926993d2db9ea7c
Ignacio Silva <ignacio.silva.e@gmail.com>
2024-09-15 15:20:37 -0300

    ✨ Add resume page and viewTransitions

--- src/utils/language.ts
+++ src/utils/language.ts
@@ -1,5 +1,31 @@
 import type { AvailableLanguageTag } from "astro/paraglide/runtime";
 
+type AbsolutePathname = `/${string}`;
+
+const pathnames: Record<
+  AbsolutePathname,
+  Record<AvailableLanguageTag, AbsolutePathname>
+> = {
+  "/resume": {
+    en: "/resume",
+    es: "/es/resume",
+  },
+  "/": {
+    en: "/",
+    es: "/es",
+  },
+};
+
+export function localizePathname(
+  pathname: AbsolutePathname,
+  locale: AvailableLanguageTag,
+) {
+  if (pathnames[pathname]) {
+    return pathnames[pathname][locale];
+  }
+  return pathname;
+}
+
 export function switchLanguage(pathname: string, locale: AvailableLanguageTag) {
   if (locale === "en") {
     return pathname.replace("es/", "");
