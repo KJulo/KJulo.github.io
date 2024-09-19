6c78e28ced916061f2ec1c4c4a960ba2a38d2785
Ignacio Silva <40589964+KJulo@users.noreply.github.com>
2024-09-19 00:04:20 -0300

    Update layout and packages (#39)
    [🔧] Update Astro
    [💄] Update loading image and alt not redundant in image profile
    [💄] Create tooltip component
    [♻️] Refactor constants front/back section, move constants files at the source of the project
    [💄] Adjust icons and backend stack
    [💄] Add 404 page
    [♿️] Add aria label in learning section and Add eslint config to no console log

---
+++ src/constants/backendStack.ts
@@ -0,0 +1,51 @@
+//Backend stack
+import NodeJs from "@icons/stack/backend/Nodejs.svg?raw";
+import ExpressJs from "@icons/stack/backend/Expressjs.svg?raw";
+import Mongodb from "@icons/stack/backend/Mongodb.svg?raw";
+import PostgreSQL from "@icons/stack/backend/PostgreSQL.svg?raw";
+import Docker from "@icons/stack/backend/Docker.svg?raw";
+import Golang from "@images/icons/stack/backend/Golang.svg?raw";
+
+export const GOLANG = {
+  title: "Golang",
+  url: "https://go.dev/",
+  icon: Golang,
+  className: "",
+};
+
+export const NODEJS = {
+  title: "NodeJs",
+  url: "https://nodejs.org/",
+  icon: NodeJs,
+  className: "",
+};
+
+export const EXPRESSJS = {
+  title: "ExpressJs",
+  url: "https://expressjs.com/",
+  icon: ExpressJs,
+  className: "dark:fill-white",
+};
+
+export const MONGODB = {
+  title: "MongoDB",
+  url: "https://www.mongodb.com/",
+  icon: Mongodb,
+  className: "",
+};
+
+export const POSTGRESQL = {
+  title: "PostgreSQL",
+  url: "https://www.postgresql.org/",
+  icon: PostgreSQL,
+  className: "",
+};
+
+export const DOCKER = {
+  title: "Docker",
+  url: "https://www.docker.com/",
+  icon: Docker,
+  className: "",
+};
+
+export const BACKEND_STACK = [NODEJS, EXPRESSJS, MONGODB, POSTGRESQL, DOCKER];
