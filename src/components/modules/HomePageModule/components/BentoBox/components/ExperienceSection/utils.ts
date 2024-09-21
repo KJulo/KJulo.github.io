1f59f5841e0348a186273161f19b66da6f29234e
Ignacio Silva <ignacio.silva.e@gmail.com>
2024-09-21 01:01:17 -0300

    💬 Adjust date for experience in messages json

--- src/components/modules/HomePageModule/components/BentoBox/components/ExperienceSection/utils.ts
+++ src/components/modules/HomePageModule/components/BentoBox/components/ExperienceSection/utils.ts
@@ -4,7 +4,10 @@ export const createExperienceList = (messages: MessagesInterface) => {
   let experienceList: {
     ROLE: string;
     COMPANY: string;
-    DATE: string;
+    DATE_START: string;
+    DATE_END: string;
+    DATE_START_RAW: string;
+    DATE_END_RAW: string;
     ACHIEVEMENTS: string[];
   }[] = [];
   for (let i = 1; messages[`experience_${i}_role`]; i++) {
@@ -13,7 +16,13 @@ export const createExperienceList = (messages: MessagesInterface) => {
       {
         ROLE: messages[`experience_${i}_role`](),
         COMPANY: messages[`experience_${i}_company`](),
-        DATE: messages[`experience_${i}_date`](),
+        DATE_START: messages[`experience_${i}_date_start`](),
+        DATE_END:
+          messages[`experience_${i}_date_end`]?.() ?? messages.date_now(),
+        DATE_START_RAW: messages[`experience_${i}_date_raw_start`](),
+        DATE_END_RAW:
+          messages[`experience_${i}_date_raw_end`]?.() ??
+          new Date().toISOString(),
         ACHIEVEMENTS: [],
       },
     ];
