bc4332659d3a08e3b3045932dc0d06de85da1455
Ignacio Silva <40589964+KJulo@users.noreply.github.com>
2025-12-18 21:57:41 -0300

    Release 3.1.0 (#71)
    * Release 3.0.0 (#66)
    
    Co-authored-by: Ignacio René Silva Elorza <ignacio.silva.e@gmail.com>
    
    * Update dependencies and update work experience
    
    * Changelog 3.1.0
    
    ---------
    
    Co-authored-by: Ignacio René Silva Elorza <ignacio.silva.e@gmail.com>

--- src/interfaces/MessagesInterface.ts
+++ src/interfaces/MessagesInterface.ts
@@ -1,3 +1,9 @@
+// En Paraglide v2, los mensajes se exportan como funciones individuales
+// La propiedad 'm' es un namespace con todos los mensajes (para auto-import)
+// Usamos Record para permitir acceso dinámico a las funciones de mensajes
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type MessageFunction = (...args: any[]) => string;
+
 export default interface MessagesInterface {
-  [key: string]: () => string;
+  [key: string]: MessageFunction;
 }
